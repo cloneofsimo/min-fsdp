3fb8463f14ec31c68af4cb099289f952ecc51f46
Simo Ryu <cloneofsimo@gmail.com>
2024-07-01 19:18:23 +0000

    mnist example

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Yes, ZeRO algorithm (used for torch FSDP and DeepSpeed) is complex. But at this point I've made minimal implementation of everything, that only logical next step is to implement ZeRO completely from scratch, with goal of reaching 0.4 MFU on h100s.
 
+# The `/journey` Directory
+
+Contains all the artifacts of the journey. Think of it more as a diary, kinda like chain of thought (hehe). `/journey` will get progressively larger and larger, all single-file implementations, which might eventually be the thing we want.
--- journey/understanding_zero/4_zero1.py
+++ journey/understanding_zero/4_zero1.py
@@ -5,9 +5,12 @@
 
 
 import torch
+import torch.nn as nn
 import torch.distributed as dist
 import os
 from basic import DummyModel, check_model_from_reference
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 
 class Zero1AdamOptimizer:
@@ -84,6 +87,11 @@ class Zero1AdamOptimizer:
             if param.grad is not None:
                 dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
 
+    def zero_grad(self):
+        for param in self.params:
+            if param.grad is not None:
+                param.grad.data.zero_()
+
     def step(self):
 
         self.reduce_all_grads()
@@ -129,7 +137,7 @@ def print0(*args, **kwargs):
         print(*args, **kwargs)
 
 
-def train():
+def train_test():
     rank = int(os.environ["LOCAL_RANK"])
     world_size = int(os.environ["WORLD_SIZE"])
 
@@ -160,5 +168,83 @@ def train():
     check_model_from_reference(model)  # Nice!
 
 
+class ConvNet(nn.Module):
+    # for mnist
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self.out = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.out(x)
+        return x
+
+
+def train_mnist():
+
+    rank = int(os.environ["LOCAL_RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+
+    print(f"Running DDP example on rank {rank}, world size: {world_size}")
+    dist.init_process_group(backend="nccl", init_method="env://")
+
+    device = f"cuda:{rank}"
+
+    model = ConvNet().to(device)
+    forward_dtype = torch.bfloat16
+    optimizer = Zero1AdamOptimizer(
+        model.parameters(), lr=2e-3, forward_dtype=forward_dtype
+    )
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+
+    trainset = datasets.MNIST(
+        root="./data", download=True, train=True, transform=transform
+    )
+
+    valset = datasets.MNIST(
+        root="./data", download=True, train=False, transform=transform
+    )
+
+    trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+    for epoch in range(1):
+        for i, (data, target) in enumerate(trainloader):
+            data, target = data.to(device, dtype=forward_dtype), target.to(device)
+
+            output = model(data)
+            loss = torch.nn.functional.cross_entropy(output, target)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            if i % 10 == 0:
+                print0(f"Epoch: {epoch}, Iter: {i}, Loss: {loss.item()}")
+
+    # eval
+    model.eval()
+    correct = 0
+    total = 0
+    valloader = DataLoader(valset, batch_size=64)
+    with torch.no_grad():
+        for data, target in valloader:
+            data, target = data.to(device, dtype=forward_dtype), target.to(device)
+            output = model(data)
+            _, predicted = torch.max(output, 1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+
+    print0(f"Accuracy: {correct/total}")
+
+
 if __name__ == "__main__":
-    train()
+    train_mnist()
